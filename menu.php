4db74718006c1d6fbe07988e403781eb0c2254e6
alheru2021daw2 <alheru2021daw2@protonmail.com>
2021-05-04 20:05:33 +0200

    .

---
+++ menu.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>
+			PÀGINA WEB DEL MENÚ PRINCIPAL DE L'APLICACIÓ D'ACCÉS A BASES DE DADES LDAP
+		</title>
+	</head>
+	<body>
+		<h2> MENÚ PRINCIPAL DE L'APLICACIÓ D'ACCÉS A BASES DE DADES LDAP</h2>
+		<h3> <b>En construcció!!!!!!!!!!!</b> </h3>
+		<a href="http://zend-alheru.fjeclot.net/final/crearUsuari.html">Creació d'usuaris</a>
+		<a href="http://zend-alheru.fjeclot.net/final/consultaUsuaris.html">Consulta d' usuaris</a>
+		<a href="http://zend-alheru.fjeclot.net/final/esborrarUsuaris.html">Esborrament d' usuaris</a>
+		<a href="http://zend-alheru.fjeclot.net/final/modUsuaris.html">Modificar usuaris</a>
+		<a href="http://zend-alheru.fjeclot.net/final/index.php">Torna a la pàgina inicial</a>
+	</body>
+</html>
